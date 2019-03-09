57ec2f63b135e88b46ddede2d635f1f45ce440e8
zqf307220692 <48276949+zqf307220692@users.noreply.github.com>
2019-03-09 23:18:38 +0800

    1
    1

--- news-manager/ news-manager-common/ src/ main/ java/ com/ zb/ entity/NewsComment.java
+++ news-manager/ news-manager-common/ src/ main/ java/ com/ zb/ entity/NewsComment.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class NewsComment implements Serializable {
     //卢克肖
     //卢卡库
-    //德赫亚
+    //鸭爸爸
     private Integer commentId; //INT PRIMARY KEY AUTO_INCREMENT,#评论编号
     private String commentContent;// VARCHAR(30) NOT NULL, #评论内容
     private String commentAuthor;// VARCHAR(30), #评论人
